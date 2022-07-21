c63a7b0bfb23272e055110bd8a9bfd88d4ace2a6
belal67 <43940222+belal67@users.noreply.github.com>
2022-07-21 11:05:27 +0200

    Add files via upload

---
+++ variable.tf
@@ -0,0 +1,23 @@
+variable "vpc" {
+  type = string
+}
+
+variable "public1" {
+  type = string
+}
+
+variable "public2" {
+  type = string
+}
+
+variable "private1" {
+  type = string
+}
+
+variable "private2" {
+  type = string
+}
+
+variable "AZ" {
+  type = string
+}
